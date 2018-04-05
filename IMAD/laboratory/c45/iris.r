9a79a21e53b7e415eac34a489891a96aa98d33ae
Piotr Bielak <piotrbielak2@gmail.com>
2018-04-05 17:50:57 +0200

    [IMAD][C45] Working...

--- IMAD/laboratory/c45/iris.r
+++ IMAD/laboratory/c45/iris.r
@@ -1,25 +1,35 @@
 library(RWeka)
 
-#iris <- read.csv('data/iris.data.txt', header=TRUE)
-#tree <- J48(Class~., data=iris)
-#plot(tree)
-#summary(tree)
-#evaluate_Weka_classifier(tree, numFolds=2)
-
-
 make_tree <- function(filepath) {
-  dataset <- read.csv(filepath, 
+  dataset <- read.csv(filepath,
                       header=TRUE,
                       colClasses = c("Class"="factor"))
-  tree <- J48(Class~., data=dataset)
+  tree <- J48(Class~.,
+              data=dataset,
+              control=Weka_control())
+  # WEKA_CONTROL PARAMETERS
+  # Use reduced error pruning: R = TRUE, FALSE  --> (DEFAULT: FALSE)
+  # Set number of folds for reduced error pruning: N = 2 | 10 --> (DEFAULT: 3)
   
-  x11()
-  plot(tree)
+  # Set minimum number of instances per leaf: M = 1 | 10 ---> (DEFAULT: 2)
+  
+  # Set confidence threshold for pruning: C = 0..1 --> (DEFAULT: 0.25)
+  
+  #x11()
+  #plot(tree)
   
   summary(tree)
+  print(tree)
+  
+  e <- evaluate_Weka_classifier(tree, numFolds = 8, class = TRUE)
+  summary(e)
+  print(e)
 }
 
 
-for(ds_filepath in c("glass", "iris", "pima-indians-diabetes", "wine")) {
-  make_tree(paste("data/", ds_filepath, ".data.txt", sep=''))  
-}
+#for(ds_filepath in c("glass", "iris", "pima-indians-diabetes", "wine")) {
+#  make_tree(paste("data/", ds_filepath, ".data.txt", sep=''))  
+#}
+
+make_tree("data/iris.data.txt")
+
