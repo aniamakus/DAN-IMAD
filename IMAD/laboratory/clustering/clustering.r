dcccdbd6d7dd04ded5e1cb613c9bb39156665a6d
Piotr Bielak <piotrbielak2@gmail.com>
2018-05-08 00:14:06 +0200

    [IMAD][Clustering] Added crossvalidation

--- IMAD/laboratory/clustering/clustering.r
+++ IMAD/laboratory/clustering/clustering.r
@@ -1,6 +1,7 @@
 # Clustering in R
 library(amap)
 library(dismo)
+library(caret)
 library(clusterCrit)
 library(cluster)
 library(fpc)
@@ -19,33 +20,34 @@ min_nb_cluster <- 1
 max_nb_cluster <- 10
 nb_classes <- length(unique(classes))
 
+perform_and_plot_cv(dataset)
 
-algorithms <- list(
-  c('KMeans-Euc', function(ds, k) Kmeans(ds, k, method='euclidean')),
-  c('KMeans-Man', function(ds, k) Kmeans(ds, k, method='manhattan')),
-  c('PAM-Euc', function(ds, k) pam(ds, k, metric='euclidean')),
-  c('PAM-Man', function(ds, k) pam(ds, k, metric='manhattan'))
-)
-
-algorithm_metrics_tuples <- evaluateAllAlgorithms(dataset_scaled, classes,
-                                                  min_nb_cluster, max_nb_cluster,
-                                                  algorithms)
-
-latex_table <- make_latex_table(algorithm_metrics_tuples, algorithms) 
-print(latex_table, 
-      include.rownames=FALSE, 
-      file=sprintf('out_tables/%s_table.tex', dataset_name))
-
-make_clust_metric_plots(algorithm_metrics_tuples, 
-                        min_nb_cluster, max_nb_cluster, 
-                        sprintf('out_plots/%s_metrics.png', dataset_name))
-
-for(alg in algorithms) {
-  make_scatter_plot(dataset_scaled, classes, 
-                    alg[[2]], nb_classes,
-                    savepath=sprintf('out_plots/%s_%s_scatter.png', dataset_name, alg[[1]]))
-  make_cluster_plot(dataset_scaled, classes,
-                    alg[[2]], nb_classes,
-                    title=alg[[1]],
-                    savepath=sprintf('out_plots/%s_%s_cluster.png', dataset_name, alg[[1]]))
-}
+# algorithms <- list(
+#   c('KMeans-Euc', function(ds, k) Kmeans(ds, k, method='euclidean')),
+#   c('KMeans-Man', function(ds, k) Kmeans(ds, k, method='manhattan')),
+#   c('PAM-Euc', function(ds, k) pam(ds, k, metric='euclidean')),
+#   c('PAM-Man', function(ds, k) pam(ds, k, metric='manhattan'))
+# )
+# 
+# algorithm_metrics_tuples <- evaluateAllAlgorithms(dataset_scaled, classes,
+#                                                   min_nb_cluster, max_nb_cluster,
+#                                                   algorithms)
+# 
+# latex_table <- make_latex_table(algorithm_metrics_tuples, algorithms) 
+# print(latex_table, 
+#       include.rownames=FALSE, 
+#       file=sprintf('out_tables/%s_table.tex', dataset_name))
+# 
+# make_clust_metric_plots(algorithm_metrics_tuples, 
+#                         min_nb_cluster, max_nb_cluster, 
+#                         sprintf('out_plots/%s_metrics.png', dataset_name))
+# 
+# for(alg in algorithms) {
+#   make_scatter_plot(dataset_scaled, classes, 
+#                     alg[[2]], nb_classes,
+#                     savepath=sprintf('out_plots/%s_%s_scatter.png', dataset_name, alg[[1]]))
+#   make_cluster_plot(dataset_scaled, classes,
+#                     alg[[2]], nb_classes,
+#                     title=alg[[1]],
+#                     savepath=sprintf('out_plots/%s_%s_cluster.png', dataset_name, alg[[1]]))
+# }
--- IMAD/laboratory/clustering/utils.r
+++ IMAD/laboratory/clustering/utils.r
@@ -108,3 +108,100 @@ make_latex_table <- function(algorithm_metric_tuples, algorithms) {
   }
   return(xtable(summaries))
 }
+
+predict_via_clustering <- function(data, classes, clusters, centroids) {
+  # Find most occuring class in cluster and assing it to the cluster
+  cluster_labels <- c()
+  for(cluster_id in unique(clusters)) {
+    cluster_classes <- classes[clusters == cluster_id]
+    current_cluster_label <- names(which.max(table(cluster_classes)))
+    cluster_labels <- append(cluster_labels, current_cluster_label)
+  }
+  
+  y_pred <- c()
+  for(idx in 1:nrow(data)) {
+    dp <- data[idx]
+    # Calc data point <-> centroids distances
+    distances <- as.matrix(dist(rbind(dp, centroids)))[1, -1]
+    # Assign the nearest cluster label as the predicted y
+    current_y <- cluster_labels[[which.min(distances)]]
+    
+    y_pred <- append(y_pred, current_y)
+  }
+  
+  return(y_pred)
+} 
+
+crossvalidate_clustering <- function(data, algorithm, nb_folds) {
+  folds <- createFolds(1:nrow(data), k=nb_folds)
+  scores_sum <- numeric(11)
+  
+  for(fold in folds) {
+    train_x <- data[-fold, ]
+    train_y <- train_x[, 'Class']
+    train_x <- scale(train_x[names(train_x) != 'Class'])
+    
+    test_x <- data[fold,]
+    y_true <- test_x[, 'Class']
+    test_x <- scale(test_x[names(test_x) != 'Class'])
+    
+    fit <- algorithm(train_x)
+    y_pred <- predict_via_clustering(test_x, train_y, fit$cluster, fit$centers)
+    
+    cm <- confusionMatrix(as.factor(y_true), as.factor(y_pred))$byClass
+    cm[is.na(cm)] <- 0
+    cm <- if (class(cm) == "matrix") colMeans(cm) else cm
+    
+    scores_sum <- scores_sum + cm
+  }
+  
+  scores <- scores_sum / nb_folds
+  
+  return(scores)
+}
+
+perform_and_plot_cv <- function(dataset) {
+  algorithm <- function(ds) Kmeans(ds, nb_classes, method='euclidean')
+  
+  min_x <- 2
+  max_x <- 10
+  
+  accs <- c()
+  precisions <- c()
+  recalls <- c()
+  f1s <- c()
+  
+  for(nb_folds in min_x:max_x) {
+    print(sprintf('Crossvalidation for nb_folds=%d', nb_folds))
+    scores <- crossvalidate_clustering(dataset, algorithm, nb_folds)
+    
+    accs <- append(accs, scores['Balanced Accuracy'])
+    precisions <- append(precisions, scores['Precision'])
+    recalls <- append(recalls, scores['Recall'])
+    f1s <- append(f1s, scores['F1'])
+  }
+  
+  x11()
+  colors <- c("red", "green", "blue", "purple", "orange")
+  par(mfrow=c(1, 4))
+  
+  plot(1, type="n", main='Balanced Accuracy', xlab="", ylab="", xlim=c(min_x, max_x), ylim=c(0,1))
+  points(min_x:max_x, accs, col=colors[[1]], pch=20)
+  lines(min_x:max_x, accs, col=colors[[1]], lty=2)
+  text(min_x:max_x, accs, round(accs, 2), pos=3)
+  
+  plot(1, type="n", main='Precision', xlab="", ylab="", xlim=c(min_x, max_x), ylim=c(0,1))
+  points(min_x:max_x, precisions, col=colors[[2]], pch=20)
+  lines(min_x:max_x, precisions, col=colors[[2]], lty=2)
+  text(min_x:max_x, precisions, round(precisions, 2), pos=3)
+  
+  plot(1, type="n", main='Recall', xlab="", ylab="", xlim=c(min_x, max_x), ylim=c(0,1))
+  points(min_x:max_x, recalls, col=colors[[3]], pch=20)
+  lines(min_x:max_x, recalls, col=colors[[3]], lty=2)
+  text(min_x:max_x, recalls, round(recalls, 2), pos=3)
+  
+  plot(1, type="n", main='F1', xlab="", ylab="", xlim=c(min_x, max_x), ylim=c(0,1))
+  points(min_x:max_x, f1s, col=colors[[4]], pch=20)
+  lines(min_x:max_x, f1s, col=colors[[4]], lty=2)
+  text(min_x:max_x, f1s, round(f1s, 2), pos=3)
+}
